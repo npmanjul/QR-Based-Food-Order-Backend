5361e999c6ae5f3231dd89401596cf335a2b1f13
anjulsingh999 <anjulsingh462@gmail.com>
2024-11-12 21:34:14 +0530

    add vercel file

--- Router/menuItem.Router.js
+++ Router/menuItem.Router.js
@@ -2,8 +2,9 @@ const express=require('express');
 const menuItemController=require('../Controller/menuItem.Controller');
 const router=express.Router();
 const authMiddleware=require('../Middleware/restaurantAuth.Middleware');
-
+// 
 router.route('/addMenuItem').post(authMiddleware,menuItemController.createMenuItem);
+
 router.route('/getMenuItem/:restaurantId').get(menuItemController.getMenuItem);
 
 module.exports=router;
